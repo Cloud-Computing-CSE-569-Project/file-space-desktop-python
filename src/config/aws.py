bc3d2ef4f4b453356d81f8ab1ff91af2770485cb
antonio-pedro99 <tonio.pedro99@gmail.com>
2022-12-10 22:53:44 +0530

    setup project and login

---
+++ src/config/aws.py
@@ -0,0 +1,17 @@
+import boto3
+import os
+import botocore as bt
+from boto3.dynamodb.conditions import Key, Attr
+import botocore.exceptions
+
+# our global configuration
+configuration = bt.config.Config(
+    region_name="us-east-2",
+    signature_version="v4",
+    retries={"max_attempts": 10, "mode": "standard"},
+)
+
+class Services:
+    dynamodb = boto3.resource("dynamodb", config=configuration)
+    cognito = boto3.client("cognito-idp")
+    cognito_id = boto3.client("cognito-identity")
