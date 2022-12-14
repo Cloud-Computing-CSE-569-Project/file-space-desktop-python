83fa68c83c45e4f3eb790e5cb62a04bad6953f0a
antonio-pedro99 <tonio.pedro99@gmail.com>
2022-12-14 15:52:04 +0530

    docker

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM python:3.9
 
 COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
-COPY ./src/ /code
+COPY ./src/ /code/src
 WORKDIR /code
 CMD ["python", "/code/src/main.py"]
--- src/config/aws.py
+++ src/config/aws.py
@@ -10,7 +10,7 @@ load_dotenv()
 USER_POOL_ID = os.getenv("USER_POOL_ID")
 BUCKET_ID = os.getenv("BUCKET_NAME")
 QUEUE_URL= os.getenv("QUEUE_URL")
-AWS_REGION = os.getenv("AWS-REGION")
+AWS_REGION = "us-east-2"
 
 # our global configuration
 configuration = bt.config.Config(
@@ -23,10 +23,10 @@ configuration = bt.config.Config(
 class Services:
     queue_url = QUEUE_URL
     dynamodb = boto3.resource("dynamodb", config=configuration)
-    cognito = boto3.client("cognito-idp")
-    cognito_id = boto3.client("cognito-identity")
+    cognito = boto3.client("cognito-idp", region_name = AWS_REGION )
+    cognito_id = boto3.client("cognito-identity", region_name = AWS_REGION)
     s3 = boto3.resource("s3")
-    client_3 = boto3.client("s3")
-    s3_bucket = s3.Bucket("")
+    client_3 = boto3.client("s3", region_name = AWS_REGION)
+    s3_bucket = s3.Bucket(BUCKET_ID)
     bucket_name = BUCKET_ID
-    sqs = boto3.client("sqs")
+    sqs = boto3.client("sqs", region_name = AWS_REGION)
